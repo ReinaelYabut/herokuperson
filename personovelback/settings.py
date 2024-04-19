e38002b04b9061973b3190d81353be4f8abd3d88
Reinae B. Yabut <142127724+ReinaelYabut@users.noreply.github.com>
2024-04-19 12:14:08 +0800

    first commit

--- personovelback/settings.py
+++ personovelback/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2zy!g&nsr&59fq#7b1x5al&i$%js!hp#6m^uukr^-g_l_8_!&h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
